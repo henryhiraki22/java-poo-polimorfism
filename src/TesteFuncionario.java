a578a865283954e9541ceacb3722ac8d65a4f986
henryhiraki22 <henry.hiraki@gmail.com>
2022-02-22 19:04:32 -0300

    first commit

---
+++ src/TesteFuncionario.java
@@ -0,0 +1,22 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		
+		Funcionario nico = new Funcionario();
+		Gerente gerente = new Gerente();
+		
+		gerente.setNome("teste");
+		gerente.setSalario(3000);
+		
+		System.out.println("A bonificação do gerente eh: " + gerente.getBonificacao());
+		
+		
+		nico.setNome("Nico Steppat");
+		nico.setCpf("12123213");
+		nico.setSalario(2600.00);
+		
+		System.out.println(nico.getNome());
+		System.out.println(nico.getBonificacao());
+	}
+}
